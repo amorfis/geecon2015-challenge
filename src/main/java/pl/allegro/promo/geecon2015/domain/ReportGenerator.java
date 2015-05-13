9bf2f821f11a3df5e70654e7f31a0618473b18f1
Paweł Stawicki <pawelstawicki@gmail.com>
2015-05-13 16:01:16 +0200

    Report generated - first test passes.

--- src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
+++ src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
@@ -3,8 +3,15 @@ package pl.allegro.promo.geecon2015.domain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.allegro.promo.geecon2015.domain.stats.FinancialStatisticsRepository;
+import pl.allegro.promo.geecon2015.domain.stats.FinancialStats;
 import pl.allegro.promo.geecon2015.domain.transaction.TransactionRepository;
+import pl.allegro.promo.geecon2015.domain.transaction.UserTransaction;
+import pl.allegro.promo.geecon2015.domain.transaction.UserTransactions;
+import pl.allegro.promo.geecon2015.domain.user.User;
 import pl.allegro.promo.geecon2015.domain.user.UserRepository;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 @Component
 public class ReportGenerator {
@@ -25,7 +32,25 @@ public class ReportGenerator {
     }
 
     public Report generate(ReportRequest request) {
+        FinancialStats stats = financialStatisticsRepository
+                .listUsersWithMinimalIncome(request.getMinimalIncome(), request.getUsersToCheck());
+
+        Stream<ReportedUser> reportedUsers = stats.getUserIds().stream().map(
+                uuid -> {
+                    User details = userRepository.detailsOf(uuid);
+
+                    UserTransactions transactions = transactionRepository.transactionsOf(uuid);
+                    BigDecimal transAmount = transactions.getTransactions()
+                            .stream()
+                            .map(UserTransaction::getAmount)
+                            .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+                    return new ReportedUser(uuid, details.getName(), transAmount);
+                });
+
         Report report = new Report();
+        reportedUsers.forEach(report::add);
+
         return report;
     }
     
